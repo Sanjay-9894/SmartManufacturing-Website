069ef00fe8b6f35caf7f11a2ac91a46ed43b195d
Sanjay-9894 <ssanjayraj150@gmail.com>
2025-03-03 21:21:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-500">
+    <h1>helloo</h1>
+    </div>
+  );
+}
